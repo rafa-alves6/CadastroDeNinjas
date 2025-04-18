5f186399a07d71e34c88c29f9d0dc9f88aaddc76
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2025-04-18 03:00:15 -0300

    Adição de anotações

--- src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -1,4 +1,14 @@
 package dev.java10x.CadastroDeNinjas;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Para sinalizar que se trata de uma class Controller
+@RequestMapping // Usado para fazer requisição de um método {GET, PUT, POST, PATCH, DELETE} (pra cada response, precisa de um get...?)
 public class Controller {
+    @GetMapping("/boasvindas") // Definição da rota (localhost:8080{ boasvindas }) *pelo apache tomcat*
+    public String boasVindas() {
+        return "Olá! Seja bem-vindo.";
+    }
 }
