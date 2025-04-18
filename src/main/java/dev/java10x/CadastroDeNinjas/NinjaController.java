d8300212c61af0bd10478738f057da59412fb5a3
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2025-04-18 17:00:09 -0300

    Cria uma entidade no Banco de Dados a partir do NinjaModel

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Para sinalizar que se trata de uma class Controller
 @RequestMapping // Usado para fazer requisição de um método {GET, PUT, POST, PATCH, DELETE} (pra cada response, precisa de um get...?)
-public class Controller {
+public class NinjaController {
     @GetMapping("/boasvindas") // Definição da rota (localhost:8080{ boasvindas }) *pelo apache tomcat*
     public String boasVindas() {
         return "Olá! Seja bem-vindo.";
